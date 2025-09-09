f7357be343c81cb4191bdd8ce252e2a0d4c349f3
pwinning1991 <pwinning1991@gmail.com>
2025-09-09 01:59:18 +0000

    use ch to read in buffer

--- main.go
+++ main.go
@@ -13,30 +13,43 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
-	currentLineContents := ""
-	for {
-		buffer := make([]byte, 8)
-		n, err := file.Read(buffer)
-		if err != nil {
-			if currentLineContents != "" {
-				fmt.Printf("read: %s\n", currentLineContents)
-				currentLineContents = ""
-			}
-			if err == io.EOF {
-				break
-			}
-			fmt.Printf("error: %s\n", err)
-			break
-		}
-		str := string(buffer[:n])
-		parts := strings.Split(str, "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
-			currentLineContents = ""
-		}
-		currentLineContents += parts[len(parts)-1]
+	linesChan := getLinesChannel(file)
+
+	for line := range linesChan {
+		fmt.Println("read:", line)
 	}
+
 
 }
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		currentLinesContents := ""
+		for {
+			buffer := make([]byte, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLinesContents != "" {
+					lines <- currentLinesContents
+				}
+				if err == io.EOF {
+					break
+				}
+				fmt.Printf("error: %s", err)
+				return
+			}
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLinesContents, parts[i])
+				currentLinesContents = ""
+			}
+			currentLinesContents += parts[len(parts)-1]
+		}
+	}()
+	return lines
+}
