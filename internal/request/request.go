1d3728f6bd1975cbc136177ca519847bccd1b8ce
pwinning1991 <ftcpjw@yahoo.com>
2025-09-09 13:28:27 -0400

    adding parsing of http request line with tests

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,82 @@
 package request
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+const crlf = "\r\n"
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	text, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	requestLine, err := parseRequestLine(text)
+	if err != nil {
+		return nil, err
+	}
+	return &Request{
+		RequestLine: *requestLine,
+	}, nil
+}
+
+func parseRequestLine(data []byte) (*RequestLine, error) {
+	idx := bytes.Index(data, []byte(crlf))
+	if idx == -1 {
+		return nil, fmt.Errorf("could not find CRLF in request-line")
+	}
+	requestLineText := string(data[:idx])
+	requestLine, err := requestLineFromString(requestLineText)
+	if err != nil {
+		return nil, err
+	}
+	return requestLine, nil
+}
+
+func requestLineFromString(requestLine string) (*RequestLine, error) {
+	parts := strings.Split(requestLine, " ")
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("could not parse request-line")
+	}
+
+	method := parts[0]
+	for _, c := range method {
+		if c < 'A' || c > 'Z' {
+			return nil, fmt.Errorf("invalid method, %s", method)
+		}
+	}
+
+	requestTarget := parts[1]
+	versionParts := strings.Split(parts[2], "/")
+	if len(versionParts) != 2 {
+		return nil, fmt.Errorf("could not parse version, %s", versionParts)
+	}
+
+	httpPart := versionParts[0]
+	if httpPart != "HTTP" {
+		return nil, fmt.Errorf("could not parse HTTP version, %s", versionParts)
+	}
+	version := versionParts[1]
+	if version != "1.1" {
+		return nil, fmt.Errorf("could not parse version, %s", version)
+	}
+
+	return &RequestLine{
+		Method:        method,
+		RequestTarget: requestTarget,
+		HttpVersion:   versionParts[1],
+	}, nil
+}
