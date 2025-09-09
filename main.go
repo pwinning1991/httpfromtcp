e0f5a8d392b2393a6bedc4a5d1108b9a17d205ba
pwinning1991 <pwinning1991@gmail.com>
2025-09-09 00:25:47 +0000

    reading from buffer

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,14 +13,30 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer file.Close()
+	currentLineContents := ""
 	for {
-		words := make([]byte, 8)
-		_, err := file.Read(words)
-		if err == io.EOF {
+		buffer := make([]byte, 8)
+		n, err := file.Read(buffer)
+		if err != nil {
+			if currentLineContents != "" {
+				fmt.Printf("read: %s\n", currentLineContents)
+				currentLineContents = ""
+			}
+			if err == io.EOF {
+				break
+			}
+			fmt.Printf("error: %s\n", err)
 			break
 		}
-		s := fmt.Sprintf("read: %s\n", words)
-		io.WriteString(os.Stdout, s)
+		str := string(buffer[:n])
+		parts := strings.Split(str, "\n")
+		for i := 0; i < len(parts)-1; i++ {
+			fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
+			currentLineContents = ""
+		}
+		currentLineContents += parts[len(parts)-1]
 	}
+
 }
 
