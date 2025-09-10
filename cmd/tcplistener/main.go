fcea689ca10b9b96cef3ee83238d45583949e7b7
pwinning1991 <ftcpjw@yahoo.com>
2025-09-09 20:19:33 -0400

    use new parsing

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io"
+	request "github.com/pwinning1991/httpfromtcp/internal/request"
 	"log"
-	"strings"
 	"net"
 )
 
@@ -20,43 +19,44 @@ func main() {
 			log.Fatal(err)
 		}
 		fmt.Println("Accpeted connection from", conn.RemoteAddr())
-		linesChan := getLinesChannel(conn)
-		for line := range linesChan {
-			fmt.Println("read:", line)
+		requestLine, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal(err)
 		}
+		fmt.Printf("Request line:\n - Method %s\n- Target: %s\n- Version: %s\n", requestLine.RequestLine.Method, requestLine.RequestLine.RequestTarget, requestLine.RequestLine.HttpVersion)
 		fmt.Println("Connection to ", conn.RemoteAddr(), "closed")
 	}
 
 }
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	lines := make(chan string)
-
-	go func() {
-		defer close(lines)
-		defer f.Close()
-		currentLinesContents := ""
-		for {
-			buffer := make([]byte, 8)
-			n, err := f.Read(buffer)
-			if err != nil {
-				if currentLinesContents != "" {
-					lines <- currentLinesContents
-				}
-				if err == io.EOF {
-					break
-				}
-				fmt.Printf("error: %s", err)
-				return
-			}
-			str := string(buffer[:n])
-			parts := strings.Split(str, "\n")
-			for i := 0; i < len(parts)-1; i++ {
-				lines <- fmt.Sprintf("%s%s", currentLinesContents, parts[i])
-				currentLinesContents = ""
-			}
-			currentLinesContents += parts[len(parts)-1]
-		}
-	}()
-	return lines
-}
+//func getLinesChannel(f io.ReadCloser) <-chan string {
+//	lines := make(chan string)
+//
+//	go func() {
+//		defer close(lines)
+//		defer f.Close()
+//		currentLinesContents := ""
+//		for {
+//			buffer := make([]byte, 8)
+//			n, err := f.Read(buffer)
+//			if err != nil {
+//				if currentLinesContents != "" {
+//					lines <- currentLinesContents
+//				}
+//				if err == io.EOF {
+//					break
+//				}
+//				fmt.Printf("error: %s", err)
+//				return
+//			}
+//			str := string(buffer[:n])
+//			parts := strings.Split(str, "\n")
+//			for i := 0; i < len(parts)-1; i++ {
+//				lines <- fmt.Sprintf("%s%s", currentLinesContents, parts[i])
+//				currentLinesContents = ""
+//			}
+//			currentLinesContents += parts[len(parts)-1]
+//		}
+//	}()
+//	return lines
+//}
